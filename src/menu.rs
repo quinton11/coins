f377850465443935e94654cbc0c285f51f5a30ba
quinton <quintons831@gmail.com>
2025-02-08 15:43:46 +0000

    feat: add-game-screen

--- src/menu.rs
+++ src/menu.rs
@@ -1,4 +1,5 @@
 use color_eyre::eyre::Result;
+use game::Game;
 use ratatui::{ layout::{Constraint, Direction, Layout}, 
 prelude::{Buffer, Rect}, style::{Color, Modifier, Style}, symbols::border, 
 text::{Line, Span}, widgets::{Block, Borders, Padding, Paragraph, StatefulWidget, Widget}, 
@@ -8,13 +9,17 @@ use crossterm::event::{self, Event, KeyCode, KeyEvent};
 
 use crate::constant;
 
+mod game;
+
 
 pub struct Menu {
     pub title: String,
     pub chest: String,
     pub page: MenuPage,
     pub mm_options: Vec<&'static str>,
-    pub selected: usize
+    pub selected: usize,
+    pub highlighted: usize,
+    pub game: Game
 }
 
 impl Default for Menu{
@@ -24,23 +29,32 @@ impl Default for Menu{
             chest: " 💰 ".to_string(), 
             page: MenuPage::Main,
             mm_options: vec!["Play - Human Mode", "Model", "Stats"],
-            selected: 0 }
+            selected: 0,
+            highlighted: 0,
+            game : Game::default() }
     }
 }
 
 #[derive(Clone)]
 pub enum MenuPage{
     Main,
-    Train,
+    Play,
+    Model,
     Stats
 }
 
-impl StatefulWidget for &mut Menu{
-    type State = MenuPage;
+pub struct GameState {
+    pub page: MenuPage,
+    pub game: Game
+}
 
-    fn render(self, area: Rect, buf: &mut Buffer, _state: &mut Self::State) {
+impl StatefulWidget for &mut Menu{
+    type State = GameState;
+
+    fn render(self, area: Rect, buf: &mut Buffer, state: &mut Self::State) {
         // draw block
         let title = Line::from(self.title.clone());
+
 
         // layout
         let layout = Layout::default()
@@ -76,24 +90,49 @@ impl StatefulWidget for &mut Menu{
         .centered()
         .block(Block::new().borders(Borders::TOP).padding(Padding::new(0, 0, 1, 0)));
 
+        chest_display.render(layout[0], buf);
+
+
+        block.render(area, buf);
+
+        match state.page {
+            MenuPage::Main => self.spawn_main_menu(layout[1], buf),
+            MenuPage::Play => self.spawn_play_screen(layout[1], buf),
+            _ => ()
+        }
+    }
+
+}
+
+impl Menu {
+    pub fn draw(&mut self, frame: &mut Frame) {
+        let page = self.page.clone();
+        let game = self.game.clone();
+        let state = &mut GameState {
+            page,
+            game
+        };
+        frame.render_stateful_widget(self, frame.area(), state);
+    }
+
+    pub fn spawn_main_menu(&mut self, area: Rect, buf: &mut Buffer) {
 
         // Coins ascii text
         let coins_text = Paragraph::new(constant::COINS)
         .centered()
         .block(Block::new().borders(Borders::ALL).padding(Padding::new(0, 0, 3, 0)));
 
-        // menu drop down will be a list of the menus
-        // we can move up and down which 
-        // loop through options, if selected attach span to
-        let menu_layout = layout[1];
+
+        let menu_layout = area;
         let y_offset = menu_layout.y + menu_layout.height/2 +2;
         let spaced: u16 = 3;
         for (i, option) in self.mm_options.iter().enumerate() {
 
-            let content = if i == self.selected {
+            let content = if i == self.highlighted {
                 Span::styled(format!("▶ {}", option),Style::default().fg(Color::Rgb(134, 47, 172)).add_modifier(Modifier::BOLD))
 
-            } else { Span::raw(*option)};
+            }
+            else { Span::raw(*option)};
 
             let text_width = content.width() as u16;
 
@@ -103,20 +142,141 @@ impl StatefulWidget for &mut Menu{
             buf.set_span(start_x, y_offset + i as u16 * spaced, &content, menu_layout.width-2);
         }
 
-        chest_display.render(layout[0], buf);
-
-        coins_text.render(layout[1], buf);
-
-
-        block.render(area, buf);
+        coins_text.render(area, buf);
     }
-}
 
-impl Menu {
-    pub fn draw(&mut self, frame: &mut Frame) {
-        let state_s = &mut self.page.clone();
-        frame.render_stateful_widget(self, frame.area(), state_s);
+    pub fn spawn_play_screen(&mut self, area: Rect, buf: &mut Buffer) {
+        // split screen horizontally into 2 with  the left one having 70% of the screen
+        let horizontal_layout = Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints(vec![Constraint::Percentage(70), Constraint::Percentage(30)])
+        .split(area);
+
+        let horizontal_left = horizontal_layout[0];
+        let horizontal_right = horizontal_layout[1];
+
+        // the left portion would be  where the game happens, right is where the action value estimates are recorded
+        // split left portion into two vertically, the top to hold the actual game and the bottom to hold the score, the steps left and steps taken and the game mode
+        let vertical_layout = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints(vec![Constraint::Percentage(80), Constraint::Percentage(20)])
+        .split(horizontal_left);
+
+        let left_top = vertical_layout[0];
+        let left_bottom = vertical_layout[1];
+
+        let start_game_color = if self.game.start {
+            Color::Green
+        } else {
+            Color::Rgb(134, 47, 172)
+        };
+
+        let top_left_text = Paragraph::new(
+            Line::from(vec![Span::styled("Game ", Style::default().fg(start_game_color))]))
+        .alignment(ratatui::layout::Alignment::Center)
+        .block(Block::new()
+        .border_type(ratatui::widgets::BorderType::Rounded)
+        .borders(Borders::ALL).border_style(Style::default().fg(start_game_color)));
+
+        
+        // check if game has started, if not display start text over game area
+        if !self.game.start {
+            let start_game_text = Span::styled("Hit 'S' to Start!",Style::default().fg(Color::Rgb(134, 47, 172)).add_modifier(Modifier::BOLD));
+
+            let text_width = start_game_text.width() as u16;
+
+            let start_x = left_top.x + (left_top.width - text_width) /2;
+            let start_y = left_top.y + (left_top.height/2);
+
+            buf.set_span(start_x, start_y, &start_game_text, left_top.width -2);
+        }
+
+        else {
+            // spawn treasure chests
+            // should be y centered, but slightly to the top
+            // each treasure box would have its index + 1 under it indicating the number corresponding to it
+
+            let y_start = left_top.y + (left_top.height/2) -2;
+            let total_chests = self.game.chests.len();
+            let spaced: u16 = 8;
+
+            for (i, _) in self.game.chests.iter().enumerate() {
+                let content = if i == self.game.highlighted_chest as usize {
+                    (
+                        Span::styled(format!("[ ❓ ]",),Style::default().fg(Color::Green)), 
+                        Span::styled(format!(" {} ", i), Style::default().fg(Color::Green))
+                    )
+                } else {
+                    (
+                        Span::styled(format!("[ ❓ ]",),Style::default().fg(Color::Rgb(134, 47, 172)).add_modifier(Modifier::BOLD)),
+                        Span::styled(format!(" {} ", i), Style::default().fg(Color::Rgb(134, 47, 172)).add_modifier(Modifier::BOLD))
+                    )
+                };
+
+                let chest_content = content.0;
+                let idx_content = content.1;
+
+                let chest_width = chest_content.width();
+                let total_width = (total_chests * chest_width) + ((total_chests - 1) * spaced as usize);
+                let start_x = left_top.x + (left_top.width.saturating_sub(total_width as u16) / 2);
+
+                let chest_x = start_x + i as u16 * (chest_width as u16 + spaced);
+
+                // content
+                buf.set_span(chest_x, y_start, &chest_content, left_top.width-1);
+                
+                // content index
+                buf.set_span(chest_x + chest_width as u16/2 - 1, y_start+ 3, &idx_content,left_top.width-1);
+            }
+
+
+            let text_content = "\u{2B05} Use direction keys to navigate \u{2B95}  ...  Hit Enter( \u{21B5} ) to Select Chest";
+            let text_width = text_content.len() as u16;
+
+            let info_text = Span::styled(
+                text_content,
+                Style::default().fg(Color::Rgb(185, 164, 56)),
+            );
+
+            // Position at bottom and center horizontally
+            let start_x = left_top.x + (left_top.width.saturating_sub(text_width) / 2);
+            let start_y = left_top.y + left_top.height.saturating_sub(2);
+
+            buf.set_span(start_x, start_y, &info_text, text_width);
+        }
+
+        // if started then user can hit chest numbers to get treasure - NEXT
+        // check if chest has been selected, if yes then get score from probabilities
+        // if chest has treasure, then update treasure chest with money emoji, else, with dust
+        // then render
+        // else if not selected, then treasure chests should have ? marks
+        // then on selection, increase step count
+        // if step count == max_steps then end of episode, store stats and display game over screen, asking to play again
+        // if not move to main screen, if yes increment episode count and start again
+
+        let bottom_left_text = Paragraph::new("Score Tab")
+        .alignment(ratatui::layout::Alignment::Center)
+        .block(Block::new().border_type(ratatui::widgets::BorderType::Rounded).borders(Borders::ALL));
+
+        let right_text = Paragraph::new("Stats Screen")
+        .alignment(ratatui::layout::Alignment::Center)
+        .block(Block::new().border_type(ratatui::widgets::BorderType::Rounded).borders(Borders::ALL));
+
+        // then simulate a game
+        // on no action taken, show an overlay text saying start, player selects a treasure box, then based on the probability of gaining a treasure of that box
+        // a reward is spat out, then the user's score is updated and his action value estimate is updated
+        // this goes on until the steps are exhausted, indicating the end of the episode
+        // when done, or game over, the stats are stored in a file, for record keeping
+        // draw 8 treasure boxes, with question marks in them at the beginning
+        // then if 
+
+        top_left_text.render(left_top, buf);
+        bottom_left_text.render(left_bottom, buf);
+        right_text.render(horizontal_right, buf);
+
+        // loop through
     }
+
 
     pub fn run(&mut self, mut terminal: DefaultTerminal) -> Result<()> {
         loop {
@@ -126,9 +286,7 @@ impl Menu {
             })?;
 
             if let Ok(event) = event::read() {
-                if let Event::Key(KeyEvent { code: KeyCode::Esc, .. }) = event {
-                    break Ok(());
-                }
+
                 match event {
                     Event::Key(KeyEvent { code: KeyCode::Down, .. }) => {
                         self.down();
@@ -136,21 +294,78 @@ impl Menu {
                     Event::Key(KeyEvent { code: KeyCode::Up, .. }) => {
                         self.up();
                     }
-                    _ => {}
+                    Event::Key(KeyEvent { code: KeyCode::Enter, .. }) => {
+                        self.select();
+                    }
+                    Event::Key(KeyEvent { code: KeyCode::Esc, .. }) => {
+                        match self.page {
+                            MenuPage::Main => break Ok(()),
+                            MenuPage::Play => {
+                                self.page = MenuPage::Main;
+                                self.game.end();
+                            },
+                            _ => ()
+                        }
+                    }
+                    Event::Key(KeyEvent { code: KeyCode::Char('s'), .. }) => {
+                        match self.page {
+                            MenuPage::Play => self.game.start(),
+                            _ => ()
+                        };
+                    }
+                    Event::Key(KeyEvent { code: KeyCode::Left, .. }) => {
+                        match self.page {
+                            MenuPage::Play => {
+                                self.game.move_left();
+                            },
+                            _ => ()
+                        }
+                    }
+                    Event::Key(KeyEvent { code: KeyCode::Right, .. }) => {
+                        match self.page {
+                            MenuPage::Play => {
+                                self.game.move_right();
+                            },
+                            _ => ()
+                        }
+                    }
+                    _ => {
+                        println!("Unhandled Key Event: {:?}", event);
+                    }
                 }
             }
         }
     }
 
     pub fn down(&mut self) {
-        if self.selected != self.mm_options.len() -1 {
-            self.selected +=1;
+        if self.highlighted != self.mm_options.len() -1 {
+            self.highlighted +=1;
         }
     }
 
     pub fn up (&mut self) {
-        if self.selected <= self.mm_options.len() -1{
-            self.selected -=1;
+        if self.highlighted > 0{
+            self.highlighted -=1;
+        }
+    }
+
+    pub fn select(&mut self) {
+        self.selected = self.highlighted;
+
+        // transition menus
+        self.next_menu()
+
+        // So if PLay is selected, we transition straight to game screen
+        // if model is selected, we transition to model menu screen, where you can play using model or train using model
+        // if stats screen is selected, then we can view model metrics, progression of weights over episodes or steps of 20 intervals, compared to real values of actions
+    }
+
+    pub fn next_menu(&mut self) {
+        self.page = match self.selected {
+            0 => MenuPage::Play,
+            1 => MenuPage::Model,
+            2 => MenuPage::Stats,
+            _ => MenuPage::Main
         }
     }
 }
---
+++ src/menu/game.rs
@@ -0,0 +1,125 @@
+use rand::prelude::*;
+
+#[derive(Clone)]
+pub struct Game {
+    pub start: bool,
+    pub score: u8,
+    pub max_steps: u8,
+    pub steps: u8,
+    pub episode: u8,
+    pub chests: Vec<f32>,
+    pub highlighted_chest: u8
+}
+
+impl Default for Game {
+    fn default() -> Self {
+        let mut game = Game {
+            start: false,
+            score: 0,
+            max_steps: 20,
+            steps: 0,
+            episode: 0,
+            chests: vec![0.0; 8],
+            highlighted_chest: 0
+        };
+
+        game.set_chest_probability();
+
+        game
+    }
+}
+
+enum GameMode {
+    Human,
+    AgentTrain,
+    AgentInfer
+}
+
+pub enum ScoreType {
+    Jackpot(i32),
+    Treasure(i32),
+    Splat(i32),
+    Loss(i32),
+    Robbed(i32)
+}
+
+impl Game {
+
+    /// Set Chest's static probabilities, these do not change for now.
+    /// Probabilities are converted from percentages:
+    /// - Chest 1: -20%  → -0.2
+    /// - Chest 2:  35%  →  0.35
+    /// - Chest 3:  10%  →  0.1
+    /// - Chest 4:  65%  →  0.65
+    /// - Chest 5: -80%  → -0.8
+    /// - Chest 6:  80%  →  0.8
+    /// - Chest 7: -30%  → -0.3 
+    /// - Chest 8: -90%  → -0.9
+    pub fn set_chest_probability(&mut self) {
+        self.chests = vec![
+            -0.2,  0.35,  0.1,  0.65,
+            -0.8,  0.8,  -0.3,  -0.9,
+        ];
+    }
+
+    /// Determines the score based on the probability of the selected chest compared to a randomly generated probability.
+    ///
+    /// - If the selected chest's probability (`chest_selected`) is greater than the random probability (`rand_prob`), the user gains:
+    ///   - **2 points** if the gap (`chest_selected - rand_prob`) is **0.5 or more** (big win).
+    ///   - **1 point** if the gap is **less than 0.5** (small win).
+    ///
+    /// - If the selected chest's probability is less than the random probability, the user loses:
+    ///   - **-2 points** if the absolute gap (`rand_prob - chest_selected`) is **0.5 or more** (big loss).
+    ///   - **-1 point** if the gap is **less than 0.5** (small loss).
+    ///
+    /// - If the probabilities match exactly (`gap == 0`), the score remains **0**.
+
+    pub fn get_chest_score(&mut self, selection: usize) -> ScoreType {
+
+        let mut rng = rand::rng();
+        let rand_prob:f32  = rng.random_range(0.0..1.0);
+
+        let chest_selected = self.chests[selection];
+
+        // Gap between chest probability and random probability
+        let gap = chest_selected - rand_prob; 
+
+        if gap > 0.0 {
+            if gap >= 0.5 {
+                ScoreType::Jackpot(2)
+            } else {
+                ScoreType::Treasure(1)
+            }
+        } else if gap < 0.0 {
+            if gap.abs() >= 0.5 {
+                ScoreType::Robbed(-2)
+            } else {
+                ScoreType::Loss(-1)
+            }
+        } else {
+            ScoreType::Splat(0)
+        }
+    }
+
+    pub fn start(&mut self) {
+        self.start=true;
+    }
+
+    pub fn end(&mut self) {
+        self.start = false;
+    }
+
+    pub fn move_right(&mut self) {
+        self.highlighted_chest = (self.highlighted_chest + 1) % self.chests.len() as u8;
+    }
+
+    pub fn move_left(&mut self) {
+        if self.highlighted_chest == 0 {
+            self.highlighted_chest = (self.chests.len() - 1) as u8;
+        } else {
+            self.highlighted_chest -= 1;
+        }
+    }
+    
+    
+}
